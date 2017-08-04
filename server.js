3c640b1052d7513c12958566fb59f8f2a097df8a
kerimlews <kerimalihodza22@gmail.com>
2017-08-04 14:13:21 +0200

    server pocetak

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-heyyy 
+var express = require('express');
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
+var path = require('path');
+var cors = require('cors');
+var passport = require('passport');
+
+const app = express();
+
+app.listen(3000, function(){
+    console.log('started')
+})
